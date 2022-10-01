80004a7edb72d3712fc1a78dd35de8899f4619ce
jzfrank <frankjinzhang@gmail.com>
2022-10-01 09:24:15 +0200

    add menu and profile image at the right side of the header

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import React from "react";
-import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import {
+  MagnifyingGlassIcon,
+  PlusCircleIcon,
+} from "@heroicons/react/24/outline";
+import { HomeIcon } from "@heroicons/react/24/solid";
 
 const Header = () => {
   return (
-    <div className="flex items-center justify-between max-w-6xl">
+    <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
       {/* Left */}
       <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
         <Image
@@ -34,7 +38,15 @@ const Header = () => {
         />
       </div>
       {/* Right */}
-      <h1>Right sides</h1>
+      <div className="flex space-x-4 items-center">
+        <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+        <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+        <img
+          src="https://pokemonletsgo.pokemon.com/assets/img/common/char-pikachu.png"
+          alt="user-image"
+          className="h-10 rounded-full cursor-pointer"
+        />
+      </div>
     </div>
   );
 };
