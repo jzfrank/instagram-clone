9c4ae8cb249ab85a5956fd79a03c30a49e7aaa0d
jzfrank <frankjinzhang@gmail.com>
2022-10-01 09:11:52 +0200

    add search bar to the header

--- components/Header.js
+++ components/Header.js
@@ -1,31 +1,40 @@
 import Image from "next/image";
 import React from "react";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
 const Header = () => {
   return (
-    <div>
+    <div className="flex items-center justify-between max-w-6xl">
       {/* Left */}
-      <div className="flex items-center justify-between max-w-6xl">
-        <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-          <Image
-            src="https://logos-world.net/wp-content/uploads/2020/04/Instagram-Logo-2010-2013.png"
-            layout="fill"
-            className="object-contain"
-          />
-        </div>
-
-        <div className="cursor-pointer h-24 w-10 relative lg:hidden">
-          <Image
-            src="https://upload.wikimedia.org/wikipedia/commons/9/95/Instagram_logo_2022.svg"
-            layout="fill"
-            className="object-contain"
-          />
-        </div>
-
-        <h1>Right sides</h1>
+      <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+        <Image
+          src="https://logos-world.net/wp-content/uploads/2020/04/Instagram-Logo-2010-2013.png"
+          layout="fill"
+          className="object-contain"
+        />
       </div>
+
+      <div className="cursor-pointer h-24 w-10 relative lg:hidden">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/9/95/Instagram_logo_2022.svg"
+          layout="fill"
+          className="object-contain"
+        />
+      </div>
+
       {/* Middle */}
+      <div className="relative mt-1">
+        <div className="absolute top-2 left-2">
+          <MagnifyingGlassIcon className="h-5 text-gray-500" />
+        </div>
+        <input
+          type="text"
+          placeholder="Search"
+          className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+        />
+      </div>
       {/* Right */}
+      <h1>Right sides</h1>
     </div>
   );
 };
