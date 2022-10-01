912eb658d9ab69caacaebb50bbd4683c8c384c69
jzfrank <frankjinzhang@gmail.com>
2022-10-01 09:31:12 +0200

    implemented a sticky header with shadow effect

--- components/Header.js
+++ components/Header.js
@@ -8,44 +8,46 @@ import { HomeIcon } from "@heroicons/react/24/solid";
 
 const Header = () => {
   return (
-    <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
-      {/* Left */}
-      <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-        <Image
-          src="https://logos-world.net/wp-content/uploads/2020/04/Instagram-Logo-2010-2013.png"
-          layout="fill"
-          className="object-contain"
-        />
-      </div>
-
-      <div className="cursor-pointer h-24 w-10 relative lg:hidden">
-        <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/9/95/Instagram_logo_2022.svg"
-          layout="fill"
-          className="object-contain"
-        />
-      </div>
-
-      {/* Middle */}
-      <div className="relative mt-1">
-        <div className="absolute top-2 left-2">
-          <MagnifyingGlassIcon className="h-5 text-gray-500" />
+    <div className="shadow-sm border-b sticky top-0 bg-white z-30">
+      <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
+        {/* Left */}
+        <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+          <Image
+            src="https://logos-world.net/wp-content/uploads/2020/04/Instagram-Logo-2010-2013.png"
+            layout="fill"
+            className="object-contain"
+          />
         </div>
-        <input
-          type="text"
-          placeholder="Search"
-          className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
-        />
-      </div>
-      {/* Right */}
-      <div className="flex space-x-4 items-center">
-        <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
-        <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
-        <img
-          src="https://pokemonletsgo.pokemon.com/assets/img/common/char-pikachu.png"
-          alt="user-image"
-          className="h-10 rounded-full cursor-pointer"
-        />
+
+        <div className="cursor-pointer h-24 w-10 relative lg:hidden">
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/9/95/Instagram_logo_2022.svg"
+            layout="fill"
+            className="object-contain"
+          />
+        </div>
+
+        {/* Middle */}
+        <div className="relative mt-1">
+          <div className="absolute top-2 left-2">
+            <MagnifyingGlassIcon className="h-5 text-gray-500" />
+          </div>
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+          />
+        </div>
+        {/* Right */}
+        <div className="flex space-x-4 items-center">
+          <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <img
+            src="https://pokemonletsgo.pokemon.com/assets/img/common/char-pikachu.png"
+            alt="user-image"
+            className="h-10 rounded-full cursor-pointer"
+          />
+        </div>
       </div>
     </div>
   );
