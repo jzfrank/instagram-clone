b656c9730accd908f9fcacbf3a02dc88a9e84475
jzfrank <frankjinzhang@gmail.com>
2022-10-04 20:56:40 +0200

    complete the signin page

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -10,6 +10,10 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+
+  pages: {
+    signin: "/auth/signin",
+  },
 };
 
 export default NextAuth(authOptions);
---
+++ pages/auth/signin.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { getProviders, signIn } from "next-auth/react";
+import Header from "../../components/Header";
+
+const signin = ({ providers }) => {
+  return (
+    <>
+      <Header />
+      <div className="flex justify-center space-x-7 mt-20">
+        <img
+          className="hidden object-cover rotate-6 md:inline-flex md:w-48"
+          src="https://www.pngkey.com/png/detail/115-1158024_instagram-phone-1.png"
+          alt="instagram-image"
+        />
+        <div className="">
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name} className="flex flex-col items-center">
+              <img
+                className="w-32 object-cover"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQY6s84djrL0r9tgFfyMUVr6xzx-pUmnvCWlw&usqp=CAU"
+                alt=""
+              />
+              <p className="font-sm italic my-10 text-center">
+                This app is created for learning purpose only.
+              </p>
+              <button
+                onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+                className="bg-red-400 rounded-lg p-3 text-white hover:bg-red-500"
+              >
+                Sign in with {provider.name}
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+// faster than useEffect
+export const getServerSideProps = async (context) => {
+  const providers = await getProviders();
+  return {
+    props: {
+      providers,
+    },
+  };
+};
+
+export default signin;
