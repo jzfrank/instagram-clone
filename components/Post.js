c864eb6577fd63236a576abfd57a666db5d04511
jzfrank <frankjinzhang@gmail.com>
2022-10-04 15:10:14 +0200

    create the post header and post image section

--- components/Post.js
+++ components/Post.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { EllipsisHorizontalIcon } from "@heroicons/react/24/outline";
 
 const Post = ({ id, username, img, userImg, caption }) => {
   return (
-    <div>
-      <h1>{username}</h1>
+    <div className="bg-white my-7 border rounded-md">
+      {/* Post Header */}
+      <div className="flex items-center p-5">
+        <img
+          src={userImg}
+          alt={username}
+          className="h-12 rounded-full object-cover border p-1"
+        />
+        <p className="font-bold flex-1">{username}</p>
+        <EllipsisHorizontalIcon className="h-5" />
+      </div>
+      {/* Post Image */}
+      <img src={img} alt="" className="object-cover w-full" />
+      <p>{caption}</p>
     </div>
   );
 };
