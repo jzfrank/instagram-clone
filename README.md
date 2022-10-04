68d6ef253cbde88380ae60694778024073a01f72
jzfrank <frankjinzhang@gmail.com>
2022-10-04 20:17:49 +0200

    install next-auth and firebase and initialize the firebase

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ Built with NextJS, TailwindCSS, and Firbase.
 
 Icons are from heroicons, user images are from minifaker.
 
+For sign in, the project used next-auth. (which uses google cloud for authorization)
+
 Also used: tailwind-scrollbar, tailwind-form.
 
 The website consists of:
---
+++ firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_apiKey,
+  authDomain: process.env.FIREBASE_authDomain,
+  projectId: process.env.FIREBASE_projectId,
+  storageBucket: process.env.FIREBASE_storageBucket,
+  messagingSenderId: process.env.FIREBASE_messagingSenderId,
+  appId: process.env.FIREBASE_appId,
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const storage = getStorage();
+
+export { app, db, storage };
